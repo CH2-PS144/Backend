e2771ee44ac765641c3437c1da51a176cb800612
Iko Raga <ikoraga@gmail.com>
2023-11-10 22:12:48 +0700

    chore: changes naming variable and file

--- src/application/web.js
+++
@@ -1,9 +0,0 @@
-const { errorMiddleware } = require("../middleware/error-middleware");
-
-const express = require("express");
-
-const app = new express();
-app.use(express.json());
-app.use(errorMiddleware);
-
-module.exports = { app };
