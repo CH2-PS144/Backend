e5e0dfce80319db3605aa8ba7b45ae542326f8ab
Iko Raga <ikoraga@gmail.com>
2023-11-12 20:34:39 +0700

    feat: changes sintax error

--- src/error/response-errror.js
+++ src/error/response-errror.js
@@ -1,7 +1,13 @@
 class ResponseError extends Error {
-  constructor(status, message) {
+  constructor(statusCode, message, isOperational = true, stack = '') {
     super(message);
-    this.status = status;
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.contructor);
+    }
   }
 }
 
