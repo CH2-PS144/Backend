bada09eb0255740b93920a1a60fdeed7dd8d8511
Iko Raga <ikoraga@gmail.com>
2023-11-12 20:28:54 +0700

    feat:fix error sintax

--- src/application/app.js
+++ src/application/app.js
@@ -1,9 +1,14 @@
 const { errorMiddleware } = require("../middleware/error-middleware");
-
+const cors = require('cors');
 const express = require("express");
+const router = require("../route/api")
 
 const app = new express();
+
+app.use(cors({ origin: '*' }));
 app.use(express.json());
+app.use(router);
 app.use(errorMiddleware);
+
 
 module.exports = { app };
