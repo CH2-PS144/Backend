ca6697288043c17d29622f184b3a5b3648930586
Iko Raga <ikoraga@gmail.com>
2023-11-12 20:36:28 +0700

    feat:changes console log

--- src/middleware/error-middleware.js
+++ src/middleware/error-middleware.js
@@ -5,28 +5,16 @@ const errorMiddleware = async (err, req, res, next) => {
   if (!err) {
     next();
     return;
-  } else if (err instanceof ResponseError) {
-    res
-      .status(err.status)
-      .json({
-        errors: err.message,
-      })
-      .end();
-  } else if (err instanceof ValidationError) {
-    res
-      .status(400)
-      .json({
-        errors: err.message,
-      })
-      .end();
-  } else {
-    res
-      .status(500)
-      .json({
-        errors: err.message,
-      })
-      .end();
   }
-};
+  if (err instanceof ResponseError) {
+    res.status(err.status).json({
+      errors: err.message
+    }).end();
+  }  else {
+    res.status(500).json({
+      errors: err.message
+    }).end();
+  }
+}
 
 module.exports = { errorMiddleware };
