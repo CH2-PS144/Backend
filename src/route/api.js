6721569a4c77afb1da5e7d3378a537adfb442a72
Iko Raga <ikoraga@gmail.com>
2023-11-15 12:50:42 +0700

    feat(api)L adding enpoint update and delete function class

--- src/route/api.js
+++ src/route/api.js
@@ -6,4 +6,6 @@ const router = new express.Router();
 router.post("/api/class", classController.createDataClassController)
 router.get("/api/class/:id",classController.getDataClassByIdController)
 router.get("/api/class",classController.getAllDataClassController)
+router.patch("/api/class/:id", classController.updateDataClassController)
+router.delete("/api/class/:id", classController.deleteDataClassController)
 module.exports = router;
