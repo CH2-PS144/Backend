f89741e25c28b839e130225a90eee7967d8428a5
Iko Raga <ikoraga@gmail.com>
2023-11-17 19:59:35 +0700

    feat(material): adding endpoint CRUD material

--- src/route/api.js
+++ src/route/api.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const classController = require("../controller/class-controller")
+const materialController = require("../controller/material-controller")
 
 const router = new express.Router();
 //Class API
@@ -9,3 +10,10 @@ router.get("/api/class",classController.getAllDataClassController)
 router.patch("/api/class/:id", classController.updateDataClassController)
 router.delete("/api/class/:id", classController.deleteDataClassController)
 module.exports = router;
+
+//Material API
+router.post("/api/materials", materialController.createDataMaterialController)
+router.get("/api/materials", materialController.getAllDataMaterialsController)
+router.get("/api/materials/:id", materialController.getDataByIdMaterialsController)
+router.patch("/api/materials/:id", materialController.updateDataMaterialsController)
+router.delete("/api/materials/:id", materialController.deleteDataMaterialController)
